ff7988a07d135d67e04f0e4debdafdc4ad5ac516
david castaño garces <david315cg@hotmail.com>
2018-12-28 15:22:51 -0500

    Se le agregan detalles como los botones a el programa

--- Datos.js
+++ Datos.js
@@ -1,10 +1,16 @@
-var trabajador =  prompt("Cual  es el nombre del trabajador");
-var horas_laboradas =  parseInt(prompt("Horas laboradas del tabajador"));
-var valor_hora =  parseInt(prompt("Valor hora del trabajador"));
+var trabajador = document.getElementById("empleado");
+var horas_laboradas = document.getElementById("horaslaboradas");
+var valor_hora = document.getElementById("valorhoras");
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click", multiplicarporClick);
 var nomina = 0;
 
-if (trabajador)
+
+function  multiplicarporClick()
 {
-  nomina = horas_laboradas * valor_hora;
-  document.write( "La nimina del trabajador " + trabajador + " es de " + nomina + " $ ");
+  var usuario = trabajador.value;
+  var numero1 = parseInt(horaslaboradas.value);
+  var numero2 = parseInt(valor_hora.value);
+  nomina = numero1 * numero2;
+  document.write( "La nomina del trabajador " + usuario + " es de " + nomina + " $ ");
 }
